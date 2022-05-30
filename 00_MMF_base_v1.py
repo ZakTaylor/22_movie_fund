101c4894d65ff8e85ab1a87a0c36e0e37aa08173
taylor.zak <taylor.zak@awatapu.school.nz>
2022-05-31 09:26:55 +1200

    base for movie fundraiser

---
+++ 00_MMF_base_v1.py
@@ -0,0 +1,32 @@
+# import statements
+
+
+# functions go here
+
+
+# --- main routine ---
+
+# set up dictionaries / lists needed to hold data
+
+# ask user if they have used the program before and show instructions if necessary
+
+# loop to get ticket details
+
+    # ask users name (cant be blank)
+
+    # ask users age (between 12 and 120)
+
+    # calculate ticket prices
+
+    # loop to ask users for snacks
+
+    # loop to ask users for drinks
+
+    # calculate snacks and drinks total price
+
+    # ask user for method of payment (apply surcharge for credit cards)
+
+
+# calculate total sales and profit
+
+# output data to text file
